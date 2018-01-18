e79a83fc6dd1ab72ff1153f6af3606178fb1b631
Marwenjf <marwenjaffel@gmail.com>
2018-01-18 20:48:44 +0100

    SpringBootMultiViewResolver

---
+++ src/main/java/org/o7planning/multiViewResolver/SpringBootMultiViewResolverApplication.java
@@ -0,0 +1,12 @@
+package org.o7planning.multiViewResolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiViewResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiViewResolverApplication.class, args);
+	}
+}
